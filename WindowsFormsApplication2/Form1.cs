6247cfb80aada1437a0d2397c1f48156cb384825
Xiaoyan Wu <xiaoyan.wu@cortlandglobal.com>
2017-06-19 16:02:20 +0800

    Add project files.

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,37 @@
+﻿using org.pdfbox.pdmodel;
+using org.pdfbox.util;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pdf2txt(new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "ShareholderStatement_EGF.pdf"), new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "test.txt"));
+        }
+
+        public static void pdf2txt(FileInfo pdffile, FileInfo txtfile)
+        {
+            PDDocument doc = PDDocument.load(pdffile.FullName);
+
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+
+            string text = pdfStripper.getText(doc);
+
+            StreamWriter swPdfChange = new StreamWriter(txtfile.FullName, false, Encoding.GetEncoding("gb2312"));
+
+            swPdfChange.Write(text);
+
+            swPdfChange.Close();
+        }
+    }
+}
